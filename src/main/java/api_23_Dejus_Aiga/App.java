1c95f5fd740e8a667af33c58a29b3cf9d346fc2a
AigaD <aiga.dejus@testdevlab.com>
2023-05-08 14:27:56 +0300

    First commint

---
+++ src/main/java/api_23_Dejus_Aiga/App.java
@@ -0,0 +1,13 @@
+package api_23_Dejus_Aiga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
